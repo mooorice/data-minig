284c78b07940da3e973a6a31ddc413250447655d
Maurice Flechtner <mauriceflechtner@gmail.com>
2024-03-22 15:34:33 +0100

    asdf

--- session1.R
+++ session1.R
@@ -1,7 +1,30 @@
- library("rvest")
- library("tidyverse")
- 
- url <-"https://www.20min.ch/"
- read_html(url) %>%
-   html_elements(css = 'a') %>%
-   html_attr('href')
+library(tidyverse)
+load("s3-subset.Rdata")
+view(dat)
+filter(dat, )
+
+class(dat)
+
+result <- lapply(dat, function(sublist) {
+  lapply(sublist, function(x) ifelse(x == "catch me if you can", TRUE, FALSE))
+})
+view(result)
+length(dat)
+class(dat[5])
+length(dat[5])
+length(dat[[5]])
+class(dat[[5]])
+length(dat[[5]][4])
+class(dat[[5]][4])
+length(dat[[5]][[4]])
+class(dat[[5]][[4]])
+length(dat[[5]][[4]][1])
+class(dat[[5]][[4]][1])
+length(dat[[5]][[4]][[1]])
+class(dat[[5]][[4]][[1]])
+dat[[5]][[4]][[1]]
+if (dat[[5]][[4]][[1]] == "catch me if you can") {
+  print("TRUE")
+} else {
+  print("FALSE")
+}
